d5e323ba8828da600ada56d11d735c26f844559b
smallest-cock <48503773+smallest-cock@users.noreply.github.com>
2023-07-05 03:35:49 -0700

    made less confusing

--- PS4_controller_example.py
+++ PS4_controller_example.py
@@ -3,25 +3,15 @@ import pyautogui
 import pygame
 from random import randint
 
-# Change/add/remove these as you wish (or dont use this list at all.... see examples below) Make sure any changes are reflected in the "edit" section below
-quickchats = [
-    'noice',
-    'dont lose this kickoff',
-    'that was totally a fake',
-    'thx brother',
-    'im gay',
-    'tell me how you really feel...',
-    'let me cook',
-    'What a tryhard!',
-    'okay buddy',
-    'im lagging',
-    'mitochondria is the powerhouse of the cell',
-    'brb uninstalling...'
-]
+
+
+# -------------------------------------------    Go to the "edit" section below to edit quickchats, macros, etc.    -----------------------------------------------------------
+
+
 
 # Create your own word variations and format them like this (see examples on how to use them in the "edit" section below)
 variations = {
-    'friend': ['homie', 'blood', 'cuh', 'dawg', 'my guy', 'my man', 'nibba', 'my dude', 'comrade', 'playa', 'fellow gamer', 'brother', 'bro', 'bruh', 'buddy', 'bud', 'fellow human'],
+    'friend': ['homie', 'blood', 'cuh', 'dawg', 'my guy', 'my man', 'nibba', 'my dude', 'comrade', 'playa', 'fellow gamer', 'brother', 'bro', 'bruh', 'buddy', 'blud', 'fellow human', 'foo', 'homie'],
     'foe': ['clown', 'nerd', 'loser', 'bozo', 'small peen', 'tard', 'cringelord', 'idiot', 'bitch', 'autist', 'dork', 'mouth breather', 'dumbass', 'virgin', 'fruitcake', 'weirdo', 'NPC'],
     'compliment': ['noice', 'awesome', 'dank', 'fire', 'impressive', 'crispy', 'beautiful', 'sexy', 'clean', 'excellent', 'superb', 'lovely', 'delightful', 'splendid', 'bussin', 'bust-worthy'],
     'cat fact': ['Cats are believed to be the only mammals who don\'t taste sweetness.',   # <-- always put a backslash ( " \ " ) before any apostrophe (to avoid the program crashing)
@@ -77,40 +67,43 @@ def resetFirstButtonPressed():
 
 # detects simultaneous button presses
 def combine(button1, button2):
-    if joysticks[0].get_button(buttons[button1]) and joysticks[0].get_button(buttons[button2]):
-        resetFirstButtonPressed()
-        return True
-    else: return False
+    global numJoysticks
+    for i in range(numJoysticks):
+        if joysticks[i].get_button(buttons[button1]) and joysticks[i].get_button(buttons[button2]):
+            resetFirstButtonPressed()
+            return True
+        else: return False
 
 # detects successive button presses (buttons pressed in a specific order)
 def sequence(button1, button2):
     global firstButtonPressed
+    global numJoysticks
     functionCallTime = time.time()
-
-    if firstButtonPressed['button'] == None:
-        if joysticks[0].get_button(buttons[button1]):
-            firstButtonPressed['time'] = functionCallTime
-            firstButtonPressed['button'] = button1
-            return False
-        else: return False
-    else:
-        if functionCallTime > (firstButtonPressed['time'] + macroTimeWindow):
-            if joysticks[0].get_button(buttons[button1]):
+    for i in range(numJoysticks):
+        if firstButtonPressed['button'] == None:
+            if joysticks[i].get_button(buttons[button1]):
                 firstButtonPressed['time'] = functionCallTime
                 firstButtonPressed['button'] = button1
                 return False
-            else:
-                resetFirstButtonPressed()
-                return False
-        else:
-            if joysticks[0].get_button(buttons[button2]):
-                if button1 == firstButtonPressed['button']:
-                    if (functionCallTime > (firstButtonPressed['time'] + 0.05)):
-                        resetFirstButtonPressed()
-                        return True
-                    else: return False
-                else: return False   
             else: return False
+        else:
+            if functionCallTime > (firstButtonPressed['time'] + macroTimeWindow):
+                if joysticks[i].get_button(buttons[button1]):
+                    firstButtonPressed['time'] = functionCallTime
+                    firstButtonPressed['button'] = button1
+                    return False
+                else:
+                    resetFirstButtonPressed()
+                    return False
+            else:
+                if joysticks[i].get_button(buttons[button2]):
+                    if button1 == firstButtonPressed['button']:
+                        if (functionCallTime > (firstButtonPressed['time'] + 0.05)):
+                            resetFirstButtonPressed()
+                            return True
+                        else: return False
+                    else: return False   
+                else: return False
 
 def quickchat(thing, chatMode='lobby', spamCount=1):
     for i in range(spamCount):
@@ -121,14 +114,16 @@ def quickchat(thing, chatMode='lobby', spamCount=1):
         time.sleep(chatSpamInterval)
 
 def toggleMacros(button):
-    if joysticks[0].get_button(buttons[button]):
-        global macrosOn
-        macrosOn = not macrosOn
-        if macrosOn:
-            print('----- quickchat macros toggled on -----\n')
-        else:
-            print('----- quickchat macros toggled off -----\n')
-        time.sleep(.2)
+    global numJoysticks
+    for i in range(numJoysticks):
+        if joysticks[i].get_button(buttons[button]):
+            global macrosOn
+            macrosOn = not macrosOn
+            if macrosOn:
+                print('----- quickchat macros toggled on -----\n')
+            else:
+                print('----- quickchat macros toggled off -----\n')
+            time.sleep(.2)
 
 def resetLastUsedVariations(key=''):
     if not (key == ''):
@@ -157,7 +152,6 @@ def variation(key):
 
 lastUsedVariations = variations.copy()
 resetLastUsedVariations()
-
 pygame.init()
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
@@ -170,68 +164,51 @@ while True:
     try:
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
+                numJoysticks = pygame.joystick.get_count()
 
-                
-# --------------------------- Edit the code below to change macros, spam amounts, chat modes, or if you made changes to the length/order of the quickchats list above -----------------------
+
+
+# ---------------------------------    Edit the code below to change quickchats, macros, spam amounts, chat modes, variations, etc.    --------------------------------------------------
+
 
                 
                 toggleMacros('ps') # <-- 'ps' is the button used to toggle on/off quick chat macros (PlayStation button)..... change as you please
 
                 if macrosOn:
 
-                    # on square + up, types 1st chat in quickchats list
+                    # on square + up, types "noice"
                     if combine('square', 'up'):
-                        quickchat(quickchats[0])
+                        quickchat('noice')
                         break
                     
-                    # on square + left, types 2nd chat in quickchats list (spamming 2 times)
+                    # on square + left, types "dont lose this kickoff" (spamming 2 times)
                     elif combine('square', 'left'):
-                        quickchat(quickchats[1], spamCount=2)  # <-- the '2' parameter is how many times the chat will be spammed.. the max you can put is 3 (before RL gives a chat timeout)
+                        quickchat('dont lose this kickoff', spamCount=2)  # <-- the '2' parameter is how many times the chat will be spammed.. the max you can put is 3 (before RL gives a chat timeout)
+                        break
+                    
+                    # on circle + up, types "tell me how you really feel..." (using team chat)
+                    elif combine('circle', 'up'):
+                        quickchat('tell me how you really feel...', chatMode='team')
+                        break
+                    
+                    # on X + right, types "im lagging" (using team chat, spamming 3 times)
+                    elif combine('x', 'right'):
+                        quickchat('im lagging', chatMode='team', spamCount=3)
                         break
 
-                    # on square + down, types 3rd chat in quickchats list
-                    elif combine('square', 'down'):
-                        quickchat(quickchats[2])
+                    # on up -> right, types "let me cook"
+                    elif sequence('up', 'right'):
+                        quickchat('let me cook')
                         break
                     
-                    # on square + right, types 4th chat in quickchats list
-                    elif combine('square', 'right'):
-                        quickchat(quickchats[3])
+                    # on up -> left, types "im gay" (using party chat)
+                    elif sequence('up', 'left'):
+                        quickchat('im gay', chatMode='party')
                         break
-                    
-                    # on circle + up, types 5th chat in quickchats list (using team chat)
-                    elif combine('circle', 'up'):
-                        quickchat(quickchats[4], chatMode='team')
-                        break
-                    
-                    # on circle + left, types 6th chat in quickchats list
-                    elif combine('circle', 'left'):
-                        quickchat(quickchats[5])
-                        break
-                    
-                    # on circle + down, types "You dont need to use the quickchats list"
-                    elif combine('circle', 'down'):
-                        quickchat('You dont need to use the quickchats list')   # <-- You can write your quick chat here if preferred (as opposed to using the quickchats list above)
-                        break
-                    
-                    # on circle + down, types 8th chat in quickchats list  ...... you get the pattern
-                    elif combine('circle', 'right'):
-                        quickchat(quickchats[7])
-                        break
-                    
+
                     # on left -> left, types "Word variations are [compliment]!"  ..... where [compliment] is a random word from the 'compliment' variations list above
                     elif sequence('left', 'left'):
                         quickchat(f'Word variations are {variation("compliment")}!')    # <-- One way to include word variations in your chats (notice the 'f' at the beginning of the string)
-                        break
-
-                    # on up -> up, types 10th chat in quickchats list (using team chat, spamming 2 times)
-                    elif sequence('up', 'up'):
-                        quickchat(quickchats[9], chatMode='team', spamCount=2)
-                        break
-                    
-                    # on up -> right, types 11th chat in quickchats list (using party chat)
-                    elif sequence('up', 'right'):
-                        quickchat(quickchats[10], chatMode='party')
                         break
 
                     # on down -> left, types "ok [foe]!!!"  ..... where [foe] is a random word from the 'foe' variations list above
@@ -244,7 +221,7 @@ while True:
                         quickchat('Wassup %s! Nice to see you again.' % variation('friend'))    # <-- Yet another way to format word variations in your chats
                         break
                     
-                    # on down -> up, types a random cat fact (from the 'cat fact' variations list above)
+                    # on down -> up, types a random cat fact (from the list of 'cat fact' variations above)
                     elif sequence('down', 'up'):
                         quickchat(variation('cat fact'))
                         break
